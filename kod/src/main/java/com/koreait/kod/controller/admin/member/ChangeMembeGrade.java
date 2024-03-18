43c5d645d69163100a21888c03c8553ac6849b28
정현진 <jhyunjin1889@gmail.com>
2024-03-18 14:32:53 +0900

    2024-03-18/정현진/회원등급변경, 모든회원조회, 등급별 회원조회 Controller추가

---
+++ kod/src/main/java/com/koreait/kod/controller/admin/member/ChangeMembeGrade.java
@@ -0,0 +1,34 @@
+package com.koreait.kod.controller.admin.member;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.koreait.kod.biz.member.MemberDTO;
+import com.koreait.kod.biz.member.MemberService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class ChangeMembeGrade {
+
+	@Autowired
+	MemberService memberService;
+	
+	public String changeMemberGrade(List<MemberDTO> memberDatas,MemberDTO memberDTO,Model model,HttpSession session) {
+		
+		MemberDTO adminDTO = (MemberDTO)session.getAttribute("adminDTO");
+		if(!adminDTO.getMemberGrade().equals("ADMIN")) {
+			return "error";
+		}
+		
+		for (MemberDTO data : memberDatas) {
+			data.setSearchCondition("changeMemberGrade");
+			memberService.update(data);
+		}
+		
+		return "adminMemberList";
+	}
+}
