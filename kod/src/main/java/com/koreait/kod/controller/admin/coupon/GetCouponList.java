de2f642ed2dd0e9b0657ecb854d0e73ce0e71731
정현진 <jhyunjin1889@gmail.com>
2024-03-18 14:30:36 +0900

    2024-03-18/정현진/등급별 쿠폰발급 Controller, 쿠폰목록조회Controller 추가

---
+++ kod/src/main/java/com/koreait/kod/controller/admin/coupon/GetCouponList.java
@@ -0,0 +1,38 @@
+package com.koreait.kod.controller.admin.coupon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.koreait.kod.biz.coupon.CouponDTO;
+import com.koreait.kod.biz.coupon.CouponService;
+import com.koreait.kod.biz.member.MemberDTO;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class GetCouponList {
+	
+	@Autowired
+	CouponService couponService;
+
+	@GetMapping
+	public String getAllCouponList(CouponDTO couponDTO,Model model,HttpSession session) {
+	
+		MemberDTO adminDTO = (MemberDTO)session.getAttribute("adminDTO");
+		if(!adminDTO.getMemberGrade().equals("ADMIN")) {
+			return "error";
+		}
+		
+		model.addAttribute("couponDatas", couponService.selectAll(couponDTO));
+		
+		
+		
+		
+		
+		return "adminCouponListPage";
+	}
+	
+	
+}
