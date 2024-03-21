ca3c5abef5549c9dd349be0cdddd70d00a2bfac2
정현진 <jhyunjin1889@gmail.com>
2024-03-21 09:56:26 +0900

    2024-03-21/정현진/★파일업로드및복사파일생성&경로생성&상품등록&리뷰등록★

--- kod/src/main/java/com/koreait/kod/controller/admin/product/InsertProduct.java
+++ kod/src/main/java/com/koreait/kod/controller/admin/product/InsertProduct.java
@@ -1,37 +1,36 @@
 package com.koreait.kod.controller.admin.product;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.koreait.kod.biz.member.MemberDTO;
-import com.koreait.kod.biz.productAndWishList.CategoryDTO;
 import com.koreait.kod.biz.productAndWishList.ImageDTO;
+import com.koreait.kod.biz.productAndWishList.ImageService;
 import com.koreait.kod.biz.productAndWishList.ProductDTO;
 import com.koreait.kod.biz.productAndWishList.ProductService;
 import com.koreait.kod.controller.user.review.FileUploadAndCopyService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-public class InsertProduct {
+public class InsertProduct { // Controller
 
 	@Autowired
 	ProductService productService;
 	@Autowired
-	FileUploadAndCopyService fileUploadAndCopy;
-	@Value("${upload.file.productImagePath}")  
-    private String uploadFilePath;
-	// @Value() -> @어노테이션 설정을 공부하기 위해 추가한 코드(절대경로사용), 
-	// 중간프로젝트 리뷰작성 로직에서 사용자환경에 따라 경로를 설정할수있도록 구현한 코드가 존재하여 다른방식으로 구현하였음
-	
+	ImageService imageService;
+	@Autowired
+    CreateFileSavePathService fileSavePathService;
+	@Autowired
+	FileUploadAndCopyService fileUploadAndCopy; // Service
+		
 	@PostMapping("/insertProduct")
 	public String insertProduct(@RequestParam("productCategory") int productCategory,
 								@RequestParam("productBrand") String productBrand,
@@ -42,10 +41,12 @@ public class InsertProduct {
 								@RequestParam("productImageName") String productImageName,
 								@RequestParam("productImageList") List<MultipartFile> productImageList,
 								ProductDTO productDTO, 
-								CategoryDTO categoryDTO,
 								ImageDTO imageDTO,
-								HttpSession session) throws IOException {
+								HttpSession session,
+								HttpServletRequest request) {
 		
+		// 관리자가 아니라면
+		// ~~~로 이동
 		if(!((MemberDTO)session.getAttribute("adminDTO")).getMemberRole().equals("ADMIN")) {
 			return "common/error";
 		}
@@ -53,16 +54,35 @@ public class InsertProduct {
 		/*
 		 * 상품등록 한글코딩
 		 * 1. 상품이미지 가져오기
-		 * 1-1. 만약 등록할 이미지가 있다면 원본파일 저장, 복사파일 생성, 복사파일 저장
-		 * 2. 상품 정보 등록 
-		 * 3. 등록완료
+		 * 2. 만약 등록할 이미지가 있다면
+		 *   2-1. 파일을 저장할 경로 얻어오기
+		 *   2-2. 원본폴더 경로에 원본파일 저장, 복사파일 생성, 복사폴더 경로에 복사파일 저장
+		 * 3. 상품 등록 
 		 * 
 		 * 상품정보를 가져오기 위해서는 ProductDTO가 필요함
 		 * 그리고 이미지등록 유무를 확인하여 파일저장 로직 수행 
 		 * 
 		 */
 		
+		// 이미지 파일이 업로드되었을 경우에만 업로드 및 파일복사 실행
+        if(productImageList != null && !productImageList.isEmpty()) {
+        	String[] filePaths = fileSavePathService.getUploadFilePath(request);
+            List<String> uuids;
+			try {
+				uuids = fileUploadAndCopy.uploadAndCopy(filePaths, productImageList);
+			} catch (IOException e) {
+				System.out.println("InsertProductController에서 오류발생");
+				e.printStackTrace();
+				
+				return "에러페이지로 이동";
+			}
+            for (String uuid : uuids) {
+            	imageDTO.setImageUrl(uuid);
+            	imageService.insert(imageDTO);
+			}
+        }
 		
+        // 상품 등록
 		productDTO.setCategoryID(productCategory);
 		productDTO.setProductBrand(productBrand);
 		productDTO.setProductName(productName);
@@ -71,60 +91,6 @@ public class InsertProduct {
 		productDTO.setProductStock(productStock);
 		productService.insert(productDTO);
 		
-		productDTO.setProductName(productName);
-		int productID = productService.selectOne(productDTO).getProductID();
-		
-		imageDTO.setProductID(productID);
-		imageDTO.setImageUrl(productImageName);
-		
-		
-		
-		
-		// 파일이 업로드되었을 경우에만 파일 처리
-		if(!(productImageList==null || productImageList.size()==0)) {
-			List<String> uuids = fileUploadAndCopy.uploadAndCopy(uploadFilePath, productImageList);
-		
-			// 첫 번째 파일에 대한 경로 생성
-			String filePath = uploadFilePath + File.separator + uuids.get(0) + "_" + productImageName;
-            File copyFile = new File(filePath);
-		
-         // 복사 파일이 존재하지 않을 경우 생성
-            if (!copyFile.exists()) { 
-                if (copyFile.createNewFile()) {
-                	imageDTO.setImageUrl(copyFile.getAbsolutePath());
-                } else {
-                    // 파일 생성 실패 시 예외 처리
-                    throw new IOException("파일생성 실패, 예외처리: " + copyFile);
-                }
-            }
-            else {// 파일이 이미 존재할 경우 처리 == 덮어쓰기
-                // 새로운 파일명 생성: 원본 파일명 + 현재 시간(밀리초 단위)을 이용하여 중복을 피함
-                String newFileName = uuids.get(0) + "_" + System.currentTimeMillis() + "_" + productImageName;
-                File newCopyFile = new File(uploadFilePath + File.separator + newFileName);
-                
-                // 새로운 파일이 존재하지 않을 경우 생성
-                if (!newCopyFile.exists()) {
-                    if (newCopyFile.createNewFile()) {
-                        // 파일 생성 성공 시 ReviewDTO의 이미지 경로를 새로운 파일의 절대 경로로 설정
-                    	imageDTO.setImageUrl(productInfo);
-                    } 
-                    else {// 파일 생성 실패 시 예외 처리
-                        throw new IOException("파일 생성 실패 : " + newCopyFile);
-                    }
-                } 
-                else {// 파일이 이미 존재할 경우 에러 메시지 반환 또는 적절한 처리
-                    throw new IOException("파일이 이미 존재 : " + newCopyFile);
-                }
-            }
-		}
-	    else {
-            imageDTO.setImageUrl(null);
-        }
-		
-		productService.insert(productDTO);
-
-		
 		return "admin/product/insertProduct";
-		
 	}
 }
--- kod/src/main/java/com/koreait/kod/controller/user/review/FileUploadAndCopyService.java
+++ kod/src/main/java/com/koreait/kod/controller/user/review/FileUploadAndCopyService.java
@@ -14,59 +14,97 @@ import org.springframework.web.multipart.MultipartFile;
 
 import net.coobird.thumbnailator.Thumbnails;
 
-@Service // @Service 을 사용하면 AOP를 지원함 => 트랜잭션 처리 + 모듈화
+@Service
 public class FileUploadAndCopyService {
 	
-	public List<String> uploadAndCopy(String uploadFilePath, List<MultipartFile> uploadReviewImages)
-			throws IOException {
-		// UUID 목록을 저장할 리스트 생성
-		List<String> uuids = new ArrayList<String>();
+    public List<String> uploadAndCopy(String[] filePaths, List<MultipartFile> uploadReviewImages)
+            throws IOException {
+    	
+        String originFilePath = filePaths[0];
+        String thumnaulFilePathForKOD = filePaths[1];
+    	
+        List<String> uuids = new ArrayList<String>(); // UUID 목록을 저장할 리스트 생성
 
-		// 업로드할 디렉토리 생성
-		File uploadDir = new File(uploadFilePath);
-		if (!uploadDir.exists()) {
-			// 디렉토리가 존재하지 않을 경우 생성
-			if (!uploadDir.mkdirs()) {
-				// 디렉토리 생성에 실패한 경우 IOException 발생
-				throw new IOException("Failed to create directory: " + uploadDir);
-			}
-		}
+        File originFileUploadDir = new File(originFilePath); // 원본파일 저장 폴더
+        File thumnailFileUploadDir = new File(thumnaulFilePathForKOD); // 복사파일 저장폴더
+        
+        // 원본폴더가 존재하지 않을 경우 생성
+        if (!(originFileUploadDir.exists() || originFileUploadDir.mkdirs())) {
+            throw new IOException("원본파일 저장폴더 생성 실패 : " + originFileUploadDir);
+        }
 
-		// 업로드된 각 이미지에 대해 처리
-		for (MultipartFile uploadReviewImage : uploadReviewImages) {
-			// UUID 생성
-			String uuid = UUID.randomUUID().toString();
-			// 업로드된 파일의 원래 이름 가져오기
-			String originalFilename = uploadReviewImage.getOriginalFilename();
-			// 파일 이름이 존재하고 비어 있지 않은 경우에만 처리
-			if (originalFilename != null && !originalFilename.isEmpty()) {
-				// UUID를 목록에 추가
-				uuids.add(uuid);
-				// 새 파일 생성
-				File file = new File(uploadDir, uuid + "_" + originalFilename);
-				try {
-					// 업로드된 파일을 새 파일로 복사
-					uploadReviewImage.transferTo(file); // 파일 복사
+        // 썸네일폴더가 존재하지 않을 경우 생성
+        if (!(thumnailFileUploadDir.exists() || thumnailFileUploadDir.mkdirs())) {
+            throw new IOException("복사파일 저장폴더 생성 실패 : " + thumnailFileUploadDir);
+        }
 
-					// 이미지인 경우에만 썸네일 생성
-					if (uploadReviewImage.getContentType() != null
-							&& uploadReviewImage.getContentType().startsWith("image")) {
-						// 썸네일 파일 생성
-						File thumbnailFile = new File(uploadDir, "t_" + uuid + "_" + originalFilename);
-						try (InputStream inputStream = uploadReviewImage.getInputStream();
-								OutputStream outputStream = new FileOutputStream(thumbnailFile)) {
-							// 이미지를 읽어와 썸네일 생성
-							Thumbnails.of(file).size(100, 100).toOutputStream(outputStream);
-						}
-					}
-				} catch (IOException e) {
-					e.printStackTrace(); // 적절한 로깅 또는 예외 처리 추가
-					// 파일 복사 실패 시 예외 처리
-				}
-			}
-		}
+        // 업로드된 각 이미지에 대해 처리
+        for (MultipartFile uploadReviewImage : uploadReviewImages) {
+            // UUID 생성
+            String uuid = UUID.randomUUID().toString();
+            // 업로드된 파일의 원래 이름 가져오기
+            String originalFilename = uploadReviewImage.getOriginalFilename();
+            // 파일 이름이 존재하고 비어 있지 않은 경우에만 처리
+            if (!(originalFilename == null || originalFilename.isEmpty())) {
+                // 새 파일 생성
+                File file = new File(originFileUploadDir, uuid + "_" + originalFilename);
+                try {
+                    // 원본파일 파일복사 실행
+                    uploadReviewImage.transferTo(file); // 파일 복사
 
-		// UUID 목록 반환
-		return uuids;
-	}
+                    // 이미지인 경우에만 썸네일 생성
+                    if (uploadReviewImage.getContentType() != null
+                            && uploadReviewImage.getContentType().startsWith("image")) {
+                        // 썸네일 파일 생성
+                        File thumbnailFile = new File(thumnailFileUploadDir, "thumbnail_" + uuid + "_" + originalFilename);
+                        try (InputStream inputStream = uploadReviewImage.getInputStream();
+                                OutputStream outputStream = new FileOutputStream(thumbnailFile)) {
+                            // 이미지를 읽어와 썸네일 생성
+                            Thumbnails.of(file).size(100, 100).toOutputStream(outputStream);
+                        }
+                        
+                        // UUID를 목록에 추가 (썸네일 파일의 UUID)
+                        uuids.add(thumnaulFilePathForKOD+File.separator+"thumbnail_" + uuid + "_" + originalFilename);
+                    } else {
+                        // 썸네일생성 실패 시 원본 파일의 UUID를 목록에 추가
+                        uuids.add(originFilePath+File.separator+uuid + "_" + originalFilename);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace(); // 적절한 로깅 또는 예외 처리 추가
+                    // 파일 복사 실패 시 예외 처리
+                }
+            }
+        }
+        // UUID 목록 반환
+        return uuids;
+    }
 }
+
+/*
+ * 썸네일을 생성한다는건
+ * 복사파일을 생성하는 행동
+ * 
+ * 원본파일은 관리자가 관리할 수 있도록 원본폴더에 저장하고
+ * 생성된 썸네일 파일은 DB에 저장하여 해당 데이터를 활용하여 사용자에게 보여줌
+ * 그리고 생성된 썸네일 파일은 썸네일 폴더에 저장 
+ * DB는 썸네일 폴더 경로에 있는 썸네일 파일을 참조함
+ * 
+ * 사용자가 사진을 삭제할 시 DB에 저장된 썸네일 파일이 삭제됨(원본파일 유지)
+ * 보안 및 유지보수 향상을 위해
+ * 원본파일은 원본폴더에서 별도로 관리함
+ * 
+ * ========================
+ * 
+ * uploadReviewImage.getContentType().startsWith("image")
+ * 해당 구문은 파일이 이미지 타입인지 확인하는 함수
+ * 파일의 확장자에 따라 MIME 타입을 유추하는 것이 일반적인 방법 중 하나입니다. 
+ * 대부분의 경우, 이미지 파일의 확장자는 이미지 형식을 나타냅니다. 
+ * 예를 들어, ".jpg", ".png", ".gif" 등의 확장자는 각각 JPEG, PNG, GIF 이미지 파일을 나타냅니다.
+ * 웹 브라우저나 다른 응용 프로그램은 주로 파일 확장자를 사용하여 파일의 MIME 타입을 추론합니다. 
+ * 따라서 "myphoto.jpg"와 같은 파일 이름을 가진 파일의 MIME 타입은 대개 "image/jpeg"로 인식됩니다. 
+ * 이것은 해당 파일이 JPEG 이미지임을 의미합니다.
+ * 그러나 확장자만으로 MIME 타입을 판별하는 것은 항상 정확하지는 않습니다. 
+ * 때때로 잘못된 확장자를 가진 파일이나 다른 형식의 파일이 이미지로 잘못 인식될 수 있습니다. 
+ * 그래서 정확한 MIME 타입을 얻기 위해서는 파일의 내용을 분석하는 것이 더 신뢰할 수 있습니다.
+ * 
+ */
