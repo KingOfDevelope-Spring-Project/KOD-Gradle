9ebefe5f86599ceda0facaeb39266c8381e00623
정현진 <jhyunjin1889@gmail.com>
2024-03-21 14:11:46 +0900

    2024-03-21/정현진/(관리자)상품등록 로직 최종완료 -  경로설정Service & 원본파일, 썸네일파일 Service제공

--- kod/src/main/java/com/koreait/kod/controller/user/review/FileUploadAndCopyService.java
+++ kod/src/main/java/com/koreait/kod/controller/user/review/FileUploadAndCopyService.java
@@ -1,6 +1,7 @@
 package com.koreait.kod.controller.user.review;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,7 +24,7 @@ public class FileUploadAndCopyService {
         String originFilePath = filePaths[0];
         String thumnaulFilePathForKOD = filePaths[1];
     	
-        List<String> uuids = new ArrayList<String>(); // UUID 목록을 저장할 리스트 생성
+        List<String> imageUrlList = new ArrayList<String>(); // UUID 목록을 저장할 리스트 생성
 
         File originFileUploadDir = new File(originFilePath); // 원본파일 저장 폴더
         File thumnailFileUploadDir = new File(thumnaulFilePathForKOD); // 복사파일 저장폴더
@@ -44,30 +45,35 @@ public class FileUploadAndCopyService {
             String uuid = UUID.randomUUID().toString();
             // 업로드된 파일의 원래 이름 가져오기
             String originalFilename = uploadReviewImage.getOriginalFilename();
-            // 파일 이름이 존재하고 비어 있지 않은 경우에만 처리
+            // 만약 파일이름이 존재한다면
             if (!(originalFilename == null || originalFilename.isEmpty())) {
-                // 새 파일 생성
+                // 원본폴더 경로에 복사할 파일 메모리 할당
                 File file = new File(originFileUploadDir, uuid + "_" + originalFilename);
                 try {
-                    // 원본파일 파일복사 실행
+                    // 원본파일 파일복사 실행, 단순 파일 복사
                     uploadReviewImage.transferTo(file); // 파일 복사
+                    System.out.println("[로그:정현진] 원본file : "+file);
 
-                    // 이미지인 경우에만 썸네일 생성
-                    if (uploadReviewImage.getContentType() != null
-                            && uploadReviewImage.getContentType().startsWith("image")) {
-                        // 썸네일 파일 생성
+                 // 이미지인 경우에만 썸네일 생성
+                    if (uploadReviewImage != null && uploadReviewImage.getContentType().startsWith("image")) {
+                        System.out.println("[로그:정현진] 썸네일 파일 생성 들어옴 ");
+                        // 썸네일 파일 생성, 이미지 조작(사이즈조절)을 위해 원본파일의 복사방법과는 다른 방식으로 진행
                         File thumbnailFile = new File(thumnailFileUploadDir, "thumbnail_" + uuid + "_" + originalFilename);
-                        try (InputStream inputStream = uploadReviewImage.getInputStream();
-                                OutputStream outputStream = new FileOutputStream(thumbnailFile)) {
+                        System.out.println("[로그:정현진] thumnailFileUploadDir : " + thumnailFileUploadDir);
+                        System.out.println("[로그:정현진] thumbnailFile : " + thumbnailFile);
+                        try (InputStream inputStream = new FileInputStream(file); // 파일의 InputStream 생성
+                             OutputStream outputStream = new FileOutputStream(thumbnailFile)) {
+                            System.out.println("[로그:정현진] 썸네일 파일복사 실행 들어옴");
                             // 이미지를 읽어와 썸네일 생성
-                            Thumbnails.of(file).size(100, 100).toOutputStream(outputStream);
+                            Thumbnails.of(inputStream).size(100, 100).toOutputStream(outputStream);
                         }
                         
-                        // UUID를 목록에 추가 (썸네일 파일의 UUID)
-                        uuids.add(thumnaulFilePathForKOD+File.separator+"thumbnail_" + uuid + "_" + originalFilename);
+                        // imageUrl를 imageUrlList에 추가 (썸네일 파일의 UUID)
+                        System.out.println("[로그:정현진] thumnaulFilePathForKOD"+thumnaulFilePathForKOD);
+                        imageUrlList.add(thumnaulFilePathForKOD+File.separator+"thumbnail_" + uuid + "_" + originalFilename);
                     } else {
-                        // 썸네일생성 실패 시 원본 파일의 UUID를 목록에 추가
-                        uuids.add(originFilePath+File.separator+uuid + "_" + originalFilename);
+                        // 썸네일생성 실패 시 원본 파일의 imageUrl을 imageUrlList에 추가
+                    	imageUrlList.add(originFilePath+File.separator+uuid + "_" + originalFilename);
                     }
                 } catch (IOException e) {
                     e.printStackTrace(); // 적절한 로깅 또는 예외 처리 추가
@@ -75,8 +81,8 @@ public class FileUploadAndCopyService {
                 }
             }
         }
-        // UUID 목록 반환
-        return uuids;
+        // imageUrlList 목록 반환
+        return imageUrlList;
     }
 }
 
@@ -108,3 +114,7 @@ public class FileUploadAndCopyService {
  * 그래서 정확한 MIME 타입을 얻기 위해서는 파일의 내용을 분석하는 것이 더 신뢰할 수 있습니다.
  * 
  */
+
+
+
+
