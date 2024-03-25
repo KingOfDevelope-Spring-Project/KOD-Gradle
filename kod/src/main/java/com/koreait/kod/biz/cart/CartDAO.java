8c2e91849c6d38330621131923b05cd388204741
HyeongRyeon <gudfus2911@gmail.com>
2024-03-25 15:51:13 +0900

    2024-03-25/조형련/cart,orderList DTO,DAO 추가

--- kod/src/main/java/com/koreait/kod/biz/cart/CartDAO.java
+++ kod/src/main/java/com/koreait/kod/biz/cart/CartDAO.java
@@ -1,39 +1,132 @@
 package com.koreait.kod.biz.cart;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository("cartDAO")
 public class CartDAO {
 	@Autowired // DI의존주입 /* @Autowired는 만능이 아니다.. 메모리에 로드가 되어있어야 가능하다. -> new JdbcTemplate();
 	private JdbcTemplate jdbcTemplate; // 의존관계 -> DI(의존주입) -> @Autowired
-
-	private static final String SELECTALL="";
-	private static final String SELECTONE="";
-	private static final String INSERT="";
-	private static final String UPDATE="";
-	private static final String DELETE="";
+	
+			// 장바구니 전체 상품 조회
+	private static final String SELECTALL="SELECT "
+			+ "C.CART_ID, "
+			+ "C.PRODUCT_ID, "
+			+ "P.PRODUCT_NAME, "
+			+ "P.PRODUCT_PRICE, "
+			+ "SUM(P.PRODUCT_PRICE * CART_PRODUCT_CNT) AS SUM_PRODUCT_PRICE, "
+			+ "C.CART_PRODUCT_CNT, "
+			+ "MIN(I.IMAGE_ID) AS IMAGE_ID,"
+			+ "(SELECT IMAGE_URL "
+			+ "FROM IMAGE "
+			+ "WHERE IMAGE_ID = MIN(I.IMAGE_ID)) AS IMAGE_URL "
+			+ "FROM CART C JOIN PRODUCT P ON P.PRODUCT_ID = C.PRODUCT_ID "
+			+ "LEFT JOIN IMAGE I ON P.PRODUCT_ID = I.PRODUCT_ID "
+			+ "WHERE C.MEMBER_ID = ? "
+			+ "GROUP BY C.CART_ID,C.PRODUCT_ID,P.PRODUCT_NAME,P.PRODUCT_PRICE";
+	
+			// 장바구니 개별상품 조회
+	private static final String SELECTONE="SELECT "
+			+ "C.CART_ID, "
+			+ "C.PRODUCT_ID, "
+			+ "P.PRODUCT_NAME, "
+			+ "P.PRODUCT_PRICE, "
+			+ "SUM(P.PRODUCT_PRICE * CART_PRODUCT_CNT) AS SUM_PRODUCT_PRICE, "
+			+ "C.CART_PRODUCT_CNT, "
+			+ "MIN(I.IMAGE_ID) AS IMAGE_ID, "
+			+ "(SELECT "
+			+ "IMAGE_URL "
+			+ "FROM IMAGE "
+			+ "WHERE IMAGE_ID = MIN(I.IMAGE_ID)) AS IMAGE_URL "
+			+ "FROM CART C "
+			+ "JOIN PRODUCT P ON P.PRODUCT_ID = C.PRODUCT_ID "
+			+ "LEFT JOIN IMAGE I ON P.PRODUCT_ID = I.PRODUCT_ID "
+			+ "WHERE C.CART_ID = ? "
+			+ "GROUP BY C.CART_ID, C.PRODUCT_ID, P.PRODUCT_NAME, P.PRODUCT_PRICE ";
+			// 장바구니 상품추가
+	private static final String INSERT="INSERT INTO CART(MEMBER_ID,PRODUCT_ID,CART_PRODUCT_CNT) VALUES (?,?,?)";
+			// 장바구니 상품 수량변경
+	private static final String UPDATE="UPDATE CART SET CART_PRODUCT_CNT = ? WHERE CART_ID=?";
+			// 장바구니 상품 개별삭제
+	private static final String DELETE_EACH_PRODUCT="DELETE "
+			+ "FROM CART "
+			+ "WHERE CART_ID=?";
+			// 장바구니 상품 삭제
+	private static final String DELETE_ALL="DELETE"
+			+ " FROM CART "
+			+ "WHERE MEMBER_ID = ?";
 
 	public List<CartDTO> selectAll(CartDTO cartDTO) {
-		return null;
+		Object[] args = { cartDTO.getMemberID() };
+		try {
+		return jdbcTemplate.query(SELECTALL, args, new CartRowMapper());
+		} 
+		catch (Exception e) {
+			return null;
+		}
 	}
-
 	public CartDTO selectOne(CartDTO cartDTO) {
-		return null;
+		Object[] args= {cartDTO.getCartID()};
+		try {
+			return jdbcTemplate.queryForObject(SELECTONE,args,new CartRowMapper());
+		}catch(Exception e) {
+			return null;
+		}
+		
 	}
-
 	public boolean insert(CartDTO cartDTO) {
-		return false;
+		int result = jdbcTemplate.update(INSERT,cartDTO.getMemberID(),cartDTO.getProductID(),cartDTO.getCartProductCnt());
+		if(result <=0) {
+			return false;			
+		}
+		return true;
 	}
 
 	public boolean update(CartDTO cartDTO) {
-		return false;
+		int result= jdbcTemplate.update(UPDATE,cartDTO.getCartProductCnt(),cartDTO.getCartID());
+		if(result<=0) {			
+			return false;
+		}
+		return true;
 	}
 
 	public boolean delete(CartDTO cartDTO) {
+		int result = 0;
+		
+		if(cartDTO.getSearchCondition().equals("DELETE_EACH_PRODUCT")) {
+			result = jdbcTemplate.update(DELETE_EACH_PRODUCT,cartDTO.getCartID());
+		}
+		else if(cartDTO.getSearchCondition().equals("DELETE_ALL")) {
+			result=jdbcTemplate.update(DELETE_ALL,cartDTO.getMemberID());
+		}
+		if(result <= 0) {
 		return false;
+		}
+		return true;
 	}
+	
+}
+
+class CartRowMapper implements RowMapper<CartDTO>{
+
+	@Override
+	public CartDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		CartDTO cartDTO = new CartDTO();
+		cartDTO.setCartID(rs.getInt("CART_ID"));
+		cartDTO.setProductID(rs.getInt("PRODUCT_ID"));
+		cartDTO.setProductName(rs.getString("PRODUCT_NAME"));
+		cartDTO.setProductPrice(rs.getInt("PRODUCT_PRICE"));
+		cartDTO.setProductTotalPrice(rs.getInt("SUM_PRODUCT_PRICE"));
+		cartDTO.setCartProductCnt(rs.getInt("CART_PRODUCT_CNT"));
+		cartDTO.setImageID(rs.getInt("IMAGE_ID"));
+		cartDTO.setProductImg(rs.getString("IMAGE_URL"));
+		return cartDTO;
+	}
+	
 }
--- kod/src/main/java/com/koreait/kod/biz/order/OrderListDAO.java
+++ kod/src/main/java/com/koreait/kod/biz/order/OrderListDAO.java
@@ -14,34 +14,60 @@ public class OrderListDAO {
 	@Autowired // DI의존주입 /* @Autowired는 만능이 아니다.. 메모리에 로드가 되어있어야 가능하다. -> new JdbcTemplate();
 	private JdbcTemplate jdbcTemplate; // 의존관계 -> DI(의존주입) -> @Autowired
 
-	private static final String SELECTALL=
-			"";
-			
+	// 주문 내역 최신순 출력
+	private static final String SELECTALL="SELECT "
+			+ "ORDERLIST_ID, "
+			+ "MEMBER_ID, "
+			+ "ORDERLIST_DATE "
+			+ "FROM ORDERLIST "
+			+ "WHERE MEMBER_ID=? "
+			+ "ORDER BY ORDERLIST_ID DESC";
+	private static final String SELECTONE_CHECK_MAX_ID="SELECT MAX(ORDERLIST_ID) AS MAX_ID FROM ORDERLIST WHERE MEMBER_ID=?";
 	private static final String SELECTONE_GET_ORDER_COUNTS_FOR_YESTERDAY_AND_TODAY=
 			"SELECT "
 			+ "    SUM(IF(DATE(ORDERLIST_DATE) = CURDATE(), 1, 0)) AS ORDER_CNT_TODAY, "
 			+ "    SUM(IF(DATE(ORDERLIST_DATE) = DATE_SUB(CURDATE(), INTERVAL 1 DAY), 1, 0)) AS ORDER_CNT_YESTERDAY "
 			+ "FROM ORDERLIST";
 	
-	private static final String INSERT="";
+	private static final String INSERT="INSERT INTO ORDERLIST (MEMBER_ID, ORDERLIST_DATE) VALUES(?, NOW())";
 	private static final String UPDATE="";
 	private static final String DELETE="";
 
 	public List<OrderListDTO> selectAll(OrderListDTO orderListDTO) {
+		Object args[] = {orderListDTO.getMemberID()};
+		try {
+			return jdbcTemplate.query(SELECTALL, args, new OrderListRowMapper());
+		}catch(Exception e) {
 			return null;
+		}
 	}
 
 	public OrderListDTO selectOne(OrderListDTO orderListDTO) {
-		if(orderListDTO.getSearchCondition().equals("orderCountsForYesterdayAndToday")){
-			return jdbcTemplate.queryForObject(SELECTONE_GET_ORDER_COUNTS_FOR_YESTERDAY_AND_TODAY, new OrderListRowMapperGetOrderCounts());
-		}
-		else {
-			return null;
-		}
+	    if (orderListDTO.getSearchCondition().equals("orderCountsForYesterdayAndToday")) {
+	        try {
+	            return jdbcTemplate.queryForObject(SELECTONE_GET_ORDER_COUNTS_FOR_YESTERDAY_AND_TODAY, new OrderListRowMapperGetOrderCounts());
+	        } catch (Exception e) {
+	            return null; 
+	        }
+	    } else if (orderListDTO.getSearchCondition().equals("check_max_id")) {
+	        try {
+	            Object[] args = { orderListDTO.getMemberID() };
+	            return jdbcTemplate.queryForObject(SELECTONE_CHECK_MAX_ID, args, new OrderListRowMapperCheckMaxID());
+	        } 
+	        catch (Exception e) {
+	            return null; 
+	        }
+	    } else {
+	        return null;
+	    }
 	}
 
 	public boolean insert(OrderListDTO orderListDTO) {
-		return false;
+		int result = jdbcTemplate.update(INSERT,orderListDTO.getMemberID());
+		if(result<=0) {
+			return false;			
+		}
+		return true;
 	}
 
 	public boolean update(OrderListDTO orderListDTO) {
@@ -66,6 +92,32 @@ class OrderListRowMapperGetOrderCounts implements RowMapper<OrderListDTO>{
 		System.out.println("어제 주문건수 : "+rs.getInt("ORDER_CNT_YESTERDAY") );
 		orderListDTO.setOrderListCntToday(rs.getInt("ORDER_CNT_TODAY"));
 		orderListDTO.setOrderListCntYesterday(rs.getInt("ORDER_CNT_YESTERDAY"));
+		return orderListDTO;
+	}
+	
+}
+
+class OrderListRowMapper implements RowMapper<OrderListDTO>{
+
+	@Override
+	public OrderListDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		OrderListDTO orderListDTO = new OrderListDTO();
+		orderListDTO.setOrderListID(rs.getInt("ORDERLIST_ID"));
+		orderListDTO.setMemberID(rs.getString("MEMBER_ID"));
+		orderListDTO.setOrderListDate(rs.getDate("ORDERLIST_DATE"));
+		
+		return orderListDTO;
+	}
+	
+}
+
+class OrderListRowMapperCheckMaxID implements RowMapper<OrderListDTO>{
+
+	@Override
+	public OrderListDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		OrderListDTO orderListDTO = new OrderListDTO();
+		orderListDTO.setOrderListID(rs.getInt("MAX_ID"));
+	
 		return orderListDTO;
 	}
 	
