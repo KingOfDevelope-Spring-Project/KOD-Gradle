63d1643d86d820c7848f8bb380813f7672053072
HyeongRyeon <gudfus2911@gmail.com>
2024-03-27 14:36:38 +0900

    2024-03-27/조형련/couponStatus DAO, DTO 추가

--- kod/src/main/java/com/koreait/kod/biz/coupon/CouponStatusDTO.java
+++ kod/src/main/java/com/koreait/kod/biz/coupon/CouponStatusDTO.java
@@ -10,8 +10,17 @@ public class CouponStatusDTO {
 	private int couponStatusID; // 쿠폰상태 PK
 	private int couponID; // 쿠폰 PK
 	private String memberID; // 회원 ID
-	private String CouponIsUsed; // 쿠폰 사용여부 체크
 	private Date couponIssueDate; // 쿠폰 발행일
 	private Date couponExpireDate; // 쿠폰 만료일
+	private int orderContentID; // 주문상세 PK
+	
+	
+	private String couponName; // 쿠폰 이름 
+	private String couponContent; // 쿠폰 설명
+	private String couponCode; // 쿠폰 코드
+	private String couponType; // 쿠폰 타입
+	private int couponDiscountRate; // 할인율
+	
+	private String searchCondition;
 	
 }
