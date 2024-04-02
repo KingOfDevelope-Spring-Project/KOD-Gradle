4c71e6c42ab15c935c01c47bced86ed2d1134657
HyeongRyeon <gudfus2911@gmail.com>
2024-04-02 15:00:24 +0900

    2024-04-02 / 조형련 / 쿠폰 발행에 따른 DTO 및 DAO 수정

--- kod/src/main/java/com/koreait/kod/biz/coupon/CouponStatusDTO.java
+++ kod/src/main/java/com/koreait/kod/biz/coupon/CouponStatusDTO.java
@@ -11,7 +11,7 @@ public class CouponStatusDTO {
 	private int couponID; // 쿠폰 PK
 	private String memberID; // 회원 ID
 	private Date couponIssueDate; // 쿠폰 발행일
-	private Date couponExpireDate; // 쿠폰 만료일
+	private int couponExpireDate; // 쿠폰 만료일
 	private int orderContentID; // 주문상세 PK
 	
 	private String searchCondition;
