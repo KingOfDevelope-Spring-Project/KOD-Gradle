71059a541cb394bd62621b0509076b1a527e3b82
정현진 <jhyunjin1889@gmail.com>
2024-04-15 09:18:23 +0900

    2024-04-15/정현진/ C 파트 결제 로직 추가

--- kod/src/main/java/com/koreait/kod/controller/user/cart/GetCartPage.java
+++ kod/src/main/java/com/koreait/kod/controller/user/cart/GetCartPage.java
@@ -1,5 +1,6 @@
 package com.koreait.kod.controller.user.cart;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,20 +8,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.koreait.kod.biz.cart.CartDTO;
 import com.koreait.kod.biz.cart.CartService;
 import com.koreait.kod.biz.member.MemberDTO;
+import com.koreait.kod.controller.util.HeaderCartService;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class GetCartPage {
 	
+	@Autowired
 	CartService cartService;
+	@Autowired
+	HeaderCartService headerCartService;
 
 	@GetMapping("/getCartPage")
 	public String getCartPage(CartDTO cartDTO,MemberDTO memberDTO,Model model,HttpSession session) {
 		
 		cartDTO.setMemberID((String)session.getAttribute("memberID"));
-		cartDTO.setSearchCondition("getCart");
 		model.addAttribute("cartDatas", cartService.selectAll(cartDTO));
+		
+		cartDTO.setMemberID((String)session.getAttribute("memberID"));
+		model.addAttribute("cartProductCnt",headerCartService.getCartProductCnt(cartDTO));
 		
 		return "user/mypage/cart";
 	}
